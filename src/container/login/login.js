a7858f85ad4840a508a0ba5088a32b855ae93d0c
tm_fannice@sina.com <tm_fannice@sina.com>
2018-06-21 20:05:15 +0800

    login register

---
+++ src/container/login/login.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import Logo from '../../component/logo/logo'
+
+import { List, InputItem, WingBlank, WhiteSpace, Button } from 'antd-mobile'
+
+class Login extends React.Component {
+  constructor(props){
+    super(props)
+    this.register = this.register.bind(this)
+  }
+  register() {
+    this.props.history.push('/register')
+  }
+  render() {
+    return(
+      <div>
+        <Logo />
+        <WingBlank>
+          <Button type='primary'>登陆</Button>
+          <WingBlank/>
+          <Button onClick={this.register} type='primary'>注册</Button>
+        </WingBlank>
+      </div>
+    )
+  }
+}
+
+export default Login
--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import registerServiceWorker from './registerServiceWorker'
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import { Provider } from 'react-redux'
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import reducers from './reducer'
+import './config'
+
+import Login from './container/login/login'
+import Register from './container/register/register'
+
+const store = createStore(reducers, applyMiddleware(thunk))
+
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <div>
+        <Route path="/login" component={Login}></Route>
+        <Route path="/register" component={Register}></Route>
+      </div>
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById('root'));
 registerServiceWorker();
